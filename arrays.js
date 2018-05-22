69b815dd0a5a64f0f68d2503bc76109a3e03ebba
Isaiah Diaz <diazisaiah0221@gmail.com>
2018-05-22 20:46:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,3 +11,18 @@ function addElementToEndOfArray(array, element) {
 function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
 }
+function accessElementInArray(array, element) {
+  console.log(array[element])
+}
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.shift(element)
+}
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array, element) {
+  array.pop(element)
+}
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1)
+}
